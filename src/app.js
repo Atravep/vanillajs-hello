9793860851433e4cf42f224bd38e3bafcaad1b20
Atravep <121689156+Atravep@users.noreply.github.com>
2024-03-24 19:48:16 +0000

    feat->Excusa con iconos y transformación en botón de reload

--- src/app.js
+++ src/app.js
@@ -6,12 +6,16 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 let who = ["Mi perro", "Un bombero", "Un kaiju", "El loco del barrio"];
-let action = ["se comió", "babeó", "robó", "bañó en salsa agridulce"];
-let what = ["mis tareas", "las llaves de mi casa", "el coche"];
+let action = ["se comió", "babeó", "robó", "quemó"];
+let what = [
+  "mis tareas",
+  "las llaves de mi casa",
+  "el coche",
+  "la tarta de cumpleaños"
+];
 let when = [
   "antes de clase",
   "tras la caída del Imperio Romano",
-  "justo cuando me desperté de la siesta",
   "mientras me duchaba",
   "justo antes de que llegase el meteorito"
 ];
@@ -22,10 +26,15 @@ function creaExcusa() {
   let what_random = what[Math.floor(Math.random() * what.length)];
   let when_random = when[Math.floor(Math.random() * when.length)];
 
-  let nuevaExcusa =
-    who_random + " " + action_random + " " + what_random + " " + when_random;
+  let nuevoWho = who_random + " ";
+  let nuevoComo = action_random + " ";
+  let nuevaCosa = what_random + " ";
+  let nuevoCuando = when_random;
 
-  document.getElementById("excuse").innerHTML = nuevaExcusa;
+  document.getElementById("quien").innerHTML = nuevoWho;
+  document.getElementById("como").innerHTML = nuevoComo;
+  document.getElementById("cosa").innerHTML = nuevaCosa;
+  document.getElementById("cuando").innerHTML = nuevoCuando;
 }
 
 window.onload = creaExcusa;
